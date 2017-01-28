24dbd922bf49bd829397987556829e6dacd4a502
Julien Pervillé <julien.perville@perfect-memory.com>
2017-01-28 14:41:42 +0100

    Annotate registry/router namespace to use region=infra node-selector

--- providers/openshift_deploy_router.rb
+++ providers/openshift_deploy_router.rb
@@ -17,6 +17,16 @@ action :create do
     mode '0644'
   end
 
+  execute 'Annotate Hosted Rouger Project' do
+    command "#{node['cookbook-openshift3']['openshift_common_client_binary']} annotate --overwrite namespace/${namespace_router} openshift.io/node-selector=${selector_router}"
+    environment(
+      'selector_router' => node['cookbook-openshift3']['openshift_hosted_router_selector'],
+      'namespace_router' => node['cookbook-openshift3']['openshift_hosted_router_namespace']
+    )
+    not_if "oc get namespace/${namespace_router} --template '{{ .metadata.annotations }}' | fgrep -q openshift.io/node-selector:${selector_router}"
+    only_if 'oc get namespace/${namespace_router} --no-headers'
+  end
+
   execute 'Deploy Hosted Router' do
     command "#{node['cookbook-openshift3']['openshift_common_client_binary']} adm router --selector=${selector_router} -n ${namespace_router} --config=admin.kubeconfig || true"
     environment(
