4d4e01ef04419c9add356da42167aa5cca333aa8
Julien Pervillé <julien.perville@perfect-memory.com>
2017-02-14 09:17:42 +0100

    Directory use /etc/origin/master/admin.kubeconfig instead of coping it.

--- providers/openshift_create_pv.rb
+++ providers/openshift_create_pv.rb
@@ -12,14 +12,9 @@ def whyrun_supported?
 end
 
 action :create do
-  remote_file "#{Chef::Config[:file_cache_path]}/admin.kubeconfig" do
-    source 'file:///etc/origin/master/admin.kubeconfig'
-    mode '0644'
-  end
-
   new_resource.persistent_storage.each do |pv|
     execute "Create Persistent Storage : #{pv['name']}" do
-      cwd Chef::Config[:file_cache_path]
+      cwd node['cookbook-openshift3']['openshift_master_config_dir']
       command 'eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolume\",\"metadata\":\{\"name\":\"${name_pv}\"\},\"spec\":\{\"capacity\":\{\"storage\":\"${capacity_pv}\"\},\"accessModes\":[\"${access_modes_pv}\"],\"nfs\":\{\"path\":\"${path_pv}\",\"server\":\"${server_pv}\"\},\"persistentVolumeReclaimPolicy\":\"${volume_policy}\"\}\}\' | oc create -f - --config=admin.kubeconfig'
       environment(
         'name_pv' => "#{pv['name']}-volume",
@@ -34,7 +29,7 @@ action :create do
 
     next unless pv.key?('claim')
     execute "Create Persistent Claim: #{pv['name']}" do
-      cwd Chef::Config[:file_cache_path]
+      cwd node['cookbook-openshift3']['openshift_master_config_dir']
       command 'eval echo \'\{\"apiVersion\":\"v1\",\"kind\":\"PersistentVolumeClaim\",\"metadata\":\{\"name\":\"${name_pvc}\"\},\"spec\":\{\"resources\":\{\"requests\":\{\"storage\":\"${capacity_pvc}\"\}\},\"accessModes\":[\"${access_modes_pvc}\"]\}\}\' | oc create -f - --config=admin.kubeconfig -n ${namespace}'
       environment(
         'name_pvc' => "#{pv['name']}-claim",
--- recipes/master_config_post.rb
+++ recipes/master_config_post.rb
@@ -8,11 +8,6 @@ master_servers = node['cookbook-openshift3']['master_servers']
 node_servers = node['cookbook-openshift3']['node_servers']
 service_accounts = node['cookbook-openshift3']['openshift_common_service_accounts_additional'].any? ? node['cookbook-openshift3']['openshift_common_service_accounts'] + node['cookbook-openshift3']['openshift_common_service_accounts_additional'] : node['cookbook-openshift3']['openshift_common_service_accounts']
 
-remote_file "#{Chef::Config[:file_cache_path]}/admin.kubeconfig" do
-  source 'file:///etc/origin/master/admin.kubeconfig'
-  mode '0644'
-end
-
 service_accounts.each do |serviceaccount|
   execute "Creation service account: \"#{serviceaccount['name']}\" ; Namespace: \"#{serviceaccount['namespace']}\"" do
     command 'oc create sa ${serviceaccount} -n ${namespace} --config=admin.kubeconfig'
@@ -20,7 +15,7 @@ service_accounts.each do |serviceaccount|
       'serviceaccount' => serviceaccount['name'],
       'namespace' => serviceaccount['namespace']
     )
-    cwd Chef::Config[:file_cache_path]
+    cwd node['cookbook-openshift3']['openshift_master_config_dir']
     not_if "#{node['cookbook-openshift3']['openshift_common_client_binary']} get sa #{serviceaccount['name']} -n #{serviceaccount['namespace']} --config=admin.kubeconfig"
   end
 
@@ -28,48 +23,48 @@ service_accounts.each do |serviceaccount|
 
   execute "Add SCC to service account: \"#{serviceaccount['name']}\" ; Namespace: \"#{serviceaccount['namespace']}\"" do
     command "#{node['cookbook-openshift3']['openshift_common_admin_binary']} policy add-scc-to-user #{serviceaccount['scc']} -z #{serviceaccount['name']} --config=admin.kubeconfig -n #{serviceaccount['namespace']}"
-    cwd Chef::Config[:file_cache_path]
+    cwd node['cookbook-openshift3']['openshift_master_config_dir']
     not_if "#{node['cookbook-openshift3']['openshift_common_client_binary']} get scc/#{serviceaccount['scc']} -n #{serviceaccount['namespace']} -o yaml --config=admin.kubeconfig | grep system:serviceaccount:#{serviceaccount['namespace']}:#{serviceaccount['name']}"
   end
 end
 
 execute 'Import Openshift Hosted Examples' do
   command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create -f #{node['cookbook-openshift3']['openshift_common_hosted_base']} --recursive --config=admin.kubeconfig -n openshift || #{node['cookbook-openshift3']['openshift_common_client_binary']} replace -f #{node['cookbook-openshift3']['openshift_common_hosted_base']} --recursive --config=admin.kubeconfig -n openshift"
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   ignore_failure true
 end
 
 execute 'Import Openshift db templates' do
   command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/db-templates --recursive --config=admin.kubeconfig -n openshift || #{node['cookbook-openshift3']['openshift_common_client_binary']} replace -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/db-templates --recursive --config=admin.kubeconfig -n openshift"
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   only_if { node['cookbook-openshift3']['deploy_example'] && node['cookbook-openshift3']['deploy_example_db_templates'] }
   ignore_failure true
 end
 
 execute 'Import Openshift Examples Base image-streams' do
   command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/image-streams/#{node['cookbook-openshift3']['openshift_base_images']} --recursive --config=admin.kubeconfig -n openshift || #{node['cookbook-openshift3']['openshift_common_client_binary']} replace -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/image-streams/#{node['cookbook-openshift3']['openshift_base_images']} --recursive --config=admin.kubeconfig -n openshift"
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   only_if { node['cookbook-openshift3']['deploy_example'] && node['cookbook-openshift3']['deploy_example_image-streams'] }
   ignore_failure true
 end
 
 execute 'Import Openshift Examples quickstart-templates' do
   command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/quickstart-templates --recursive --config=admin.kubeconfig -n openshift"
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   only_if { node['cookbook-openshift3']['deploy_example'] && node['cookbook-openshift3']['deploy_example_quickstart-templates'] }
   ignore_failure true
 end
 
 execute 'Import Openshift Examples xpaas-streams' do
   command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/xpaas-streams --recursive --config=admin.kubeconfig -n openshift"
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   only_if { node['cookbook-openshift3']['deploy_example'] && node['cookbook-openshift3']['deploy_example_xpaas-streams'] }
   ignore_failure true
 end
 
 execute 'Import Openshift Examples xpaas-templates' do
   command "#{node['cookbook-openshift3']['openshift_common_client_binary']} create -f #{node['cookbook-openshift3']['openshift_common_examples_base']}/xpaas-templates --recursive --config=admin.kubeconfig -n openshift"
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   only_if { node['cookbook-openshift3']['deploy_example'] && node['cookbook-openshift3']['deploy_example_xpaas-templates'] }
   ignore_failure true
 end
@@ -84,7 +79,7 @@ node_servers.each do |nodes|
     environment(
       'schedulability' => !nodes.key?(:schedulable) && master_servers.find { |server_node| server_node['fqdn'] == nodes['fqdn'] } ? 'False' : nodes['schedulable'].to_s
     )
-    cwd Chef::Config[:file_cache_path]
+    cwd node['cookbook-openshift3']['openshift_master_config_dir']
     only_if do
       master_servers.find { |server_node| server_node['fqdn'] == nodes['fqdn'] } &&
         !Mixlib::ShellOut.new("oc get node | grep #{nodes['fqdn']}").run_command.error?
@@ -96,7 +91,7 @@ node_servers.each do |nodes|
     environment(
       'schedulability' => !nodes.key?(:schedulable) && node_servers.find { |server_node| server_node['fqdn'] == nodes['fqdn'] } ? 'True' : nodes['schedulable'].to_s
     )
-    cwd Chef::Config[:file_cache_path]
+    cwd node['cookbook-openshift3']['openshift_master_config_dir']
     not_if do
       master_servers.find { |server_node| server_node['fqdn'] == nodes['fqdn'] } ||
         Mixlib::ShellOut.new("oc get node | grep #{nodes['fqdn']}").run_command.error?
@@ -108,7 +103,7 @@ node_servers.each do |nodes|
     environment(
       'labels' => nodes['labels'].to_s
     )
-    cwd Chef::Config[:file_cache_path]
+    cwd node['cookbook-openshift3']['openshift_master_config_dir']
     only_if do
       nodes.key?('labels') &&
         !Mixlib::ShellOut.new("oc get node | grep #{nodes['fqdn']}").run_command.error?
@@ -118,7 +113,7 @@ end
 
 execute 'Wait up to 30s for nodes registration' do
   command '[[ `oc get node --no-headers --config=admin.kubeconfig | grep -wc "Ready"` -ne 0 ]]'
-  cwd Chef::Config[:file_cache_path]
+  cwd node['cookbook-openshift3']['openshift_master_config_dir']
   only_if '[[ `oc get node --no-headers --config=admin.kubeconfig | grep -wc "Ready"` -eq 0 ]]'
   retries 6
   retry_delay 5
@@ -143,9 +138,4 @@ openshift_deploy_metrics 'Deploy Cluster Metrics' do
   only_if do
     node['cookbook-openshift3']['openshift_hosted_cluster_metrics']
   end
-end
-
-file "#{Chef::Config[:file_cache_path]}/admin.kubeconfig" do
-  action :delete
-  only_if { File.exist? "#{Chef::Config[:file_cache_path]}/admin.kubeconfig" }
 end
